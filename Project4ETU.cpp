882024d1b7d0e7c3c5b8cd0bd69203199533db76
HostCase <430ap430@gmail.com>
2023-12-22 22:38:15 +0300

    punctuation

--- Project4ETU.cpp
+++ Project4ETU.cpp
@@ -3,26 +3,47 @@
 #include <fstream>
 #include <windows.h>
 #include <unordered_map>
+#include <sstream>
 using namespace std;
+const int maxWords = 50;
+const int maxWordLength = 10;
 void processString(const string& str, string& text, int &filein);
-
+void removeExtraSpaces(string& text);
+void limitWordsLength(string& text);
+void removeExtraPunct(string& text);
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "");
     string text;
     string choice;
-    int filein = 0;
+    int filein = 0, whatyouwant = 0;
     cout << "Choose what we will do (from file - enter file)/(from console - enter console)\n";
     getline(cin, choice);
     do {
         if (!(filein)) {
             processString(choice, text, filein);
         }
-        cout<<"What do you want to do with the file?\n";
-
-
-    } while (!(choice=="exit"));
+        cout<<"What do you want to do with the file?\n0 - exit\n1 - Remove spaces\n2 - Remove extra punctuation\n5 - show text\n";
+        cin >> whatyouwant;
+        switch (whatyouwant) {
+        case 1: {
+            cout << "Removing spaces\n";
+            removeExtraSpaces(text);
+            cout << "Completed\n";
+            break;
+        }
+        case 2: {
+            cout << "Removing extra punctuation\n";
+            removeExtraPunct(text);
+            cout << "Completed\n";
+            break;
+        }
+        case 5: {
+            cout <<"Your text is "<< text << "\n";
+        }
+        }
+    } while (whatyouwant);
     return 0;
 }
 
@@ -54,6 +75,7 @@ void processString(const string& str,string &text,int &filein ) {
                 while (getline(fin, line)) {
                     text += line + " ";
                 }
+                limitWordsLength(text);
 
             }
             filein = 1;
@@ -63,6 +85,7 @@ void processString(const string& str,string &text,int &filein ) {
         case 2: {
             cout << "Enter the string: \n";
             getline(cin, text);
+            limitWordsLength(text);
             filein = 1;
             break;
         }
@@ -70,4 +93,83 @@ void processString(const string& str,string &text,int &filein ) {
             cout << "Unknown type\n";
         }
     }
+}
+
+
+void removeExtraSpaces(string& text) {
+    string output;
+    bool previousIsSpace = false;
+
+    for (char c : text) {
+        if (isspace(c)) {
+            if (previousIsSpace) {
+                continue;
+            }
+            else {
+                output += ' ';
+                previousIsSpace = true;
+            }
+        }
+        else {
+            output += c;
+            previousIsSpace = false;
+        }
+    }
+    output.erase(output.find_last_not_of(" \n\r\t") + 1);
+    output.erase(0, output.find_first_not_of(" \n\r\t"));
+
+    text = output;
+}
+
+void limitWordsLength(string& text) {
+    istringstream stream(text);
+    string word;
+    string* words = new string[maxWords]; 
+    int wordCount = 0;
+
+    while (stream >> word) {
+        if (word.length() > maxWordLength) {
+            word = word.substr(0, maxWordLength);
+        }
+        if (wordCount < maxWords) {
+            words[wordCount] = word;
+            wordCount++;
+        }
+        else {
+            break;
+        }
+    }
+    text = "";
+   
+    
+    for (int i = 0; i < wordCount; i++) {
+        text += words[i] + " ";
+    }
+    text.pop_back(); 
+
+    delete[] words; // ПРОГРАММИРУЕМ КАК УМЕЕМ
+}
+
+
+
+void removeExtraPunct(string& text) {
+    string output;
+    bool previousIsPunctuation = false;
+
+    for (char c : text) {
+        if (ispunct(c) && !(c=='.')) {
+            if (previousIsPunctuation) {
+            }
+            else {
+                output += c;
+                previousIsPunctuation = true;
+            }
+        }
+        else {
+            output += c;
+            previousIsPunctuation = false;
+        }
+    }
+
+    text = output;
 }
